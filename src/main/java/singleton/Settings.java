13ac6b709f0a5bfc1607169a2f6c6f647b0bd456
최선혁 <wafy75@gmail.com>
2022-11-07 16:56:39 +0900

    static inner class 사용하기

--- src/main/java/singleton/Settings.java
+++ src/main/java/singleton/Settings.java
@@ -2,13 +2,16 @@ package singleton;
 
 public class Settings {
 
-    private static volatile Settings instance;
-
     /**
      * new를 사용해서 외부에서 객체를 생성하지 못한다.
      */
     private Settings() {
     }
+
+    private static class SettingsHolder {
+        private static final Settings INSTANCE = new Settings();
+    }
+
 
     /**
      * 두가지 쓰레드가 동시에 if문을 접근하면 쓰레드가 두개 생성되는 문제점.
@@ -20,15 +23,11 @@ public class Settings {
      * - 동시에 쓰레드가 접근하더래도 if문 안에서 동기화를 처리하기 때문에 메서드에 걸리는것보다 성능의 유리
      * - 인스턴스가필요한 시점에 생성하는점
      * - jdk 1.5부터 동작함
+     * 4. static inner 클래스 사용하기
+     * - 권고하는 방법중 하나
      */
+
     public static Settings getInstance() {
-        if (instance == null) {
-            synchronized (Settings.class) {
-                if (instance == null) {
-                    instance = new Settings();
-                }
-            }
-        }
-        return instance;
+        return SettingsHolder.INSTANCE;
     }
 }
