a95964001cfdb8b762496798a396f79fe00401a3
최선혁 <wafy75@gmail.com>
2022-11-07 16:49:54 +0900

    이른 초기화 설정

--- src/main/java/singleton/Settings.java
+++ src/main/java/singleton/Settings.java
@@ -2,7 +2,7 @@ package singleton;
 
 public class Settings {
 
-    private static Settings instance;
+    private static final Settings INSTANCE = new Settings();
 
     /**
      * new를 사용해서 외부에서 객체를 생성하지 못한다.
@@ -12,12 +12,12 @@ public class Settings {
 
     /**
      * 두가지 쓰레드가 동시에 if문을 접근하면 쓰레드가 두개 생성되는 문제점.
-     * 1.synchronized : 동기화 메커니즘 때문에 lock발생때문에 성능에 문제가 발생한다.
+     * 1. synchronized : 동기화 메커니즘 때문에 lock 발생 때문에 성능에 문제가 발생한다.
+     * 2. 이른 초기화(eager initialization) 사용하기
+     * - 미리 만드는게 단점
+     * - 객체 생성단계가 오래 걸리는데 만들어 놓고 사용하지 않는다면 비효율
      */
-    public static synchronized Settings getInstance() {
-        if (instance == null) {
-            instance = new Settings();
-        }
-        return instance;
+    public static Settings getInstance() {
+        return INSTANCE;
     }
 }
