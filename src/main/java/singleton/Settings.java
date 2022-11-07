c7cb3d8f2aabd79d2f47a32ec63f91d6aeb6b3ce
최선혁 <wafy75@gmail.com>
2022-11-07 16:46:51 +0900

    synchronized 사용방법

--- src/main/java/singleton/Settings.java
+++ src/main/java/singleton/Settings.java
@@ -10,7 +10,11 @@ public class Settings {
     private Settings() {
     }
 
-    public static Settings getInstance() {
+    /**
+     * 두가지 쓰레드가 동시에 if문을 접근하면 쓰레드가 두개 생성되는 문제점.
+     * 1.synchronized : 동기화 메커니즘 때문에 lock발생때문에 성능에 문제가 발생한다.
+     */
+    public static synchronized Settings getInstance() {
         if (instance == null) {
             instance = new Settings();
         }
