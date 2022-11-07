fc5ae1155bf42acd6368c8a3ea4c1a547301914d
최선혁 <wafy75@gmail.com>
2022-11-07 16:54:23 +0900

    더블체크드 락킹 사용1

--- src/main/java/singleton/Settings.java
+++ src/main/java/singleton/Settings.java
@@ -2,7 +2,7 @@ package singleton;
 
 public class Settings {
 
-    private static final Settings INSTANCE = new Settings();
+    private static volatile Settings instance;
 
     /**
      * new를 사용해서 외부에서 객체를 생성하지 못한다.
@@ -16,8 +16,19 @@ public class Settings {
      * 2. 이른 초기화(eager initialization) 사용하기
      * - 미리 만드는게 단점
      * - 객체 생성단계가 오래 걸리는데 만들어 놓고 사용하지 않는다면 비효율
+     * 3. 더블 체크드 락킹 사용하기
+     * - 동시에 쓰레드가 접근하더래도 if문 안에서 동기화를 처리하기 때문에 메서드에 걸리는것보다 성능의 유리
+     * - 인스턴스가필요한 시점에 생성하는점
+     * - jdk 1.5부터 동작함
      */
     public static Settings getInstance() {
-        return INSTANCE;
+        if (instance == null) {
+            synchronized (Settings.class) {
+                if (instance == null) {
+                    instance = new Settings();
+                }
+            }
+        }
+        return instance;
     }
 }
