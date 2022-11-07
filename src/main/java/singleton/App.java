b543aef53992a035a9ac468aafc4004112753e1b
최선혁 <wafy75@gmail.com>
2022-11-07 17:18:14 +0900

    enum을 사용하기 - 가장 권고하는 방법

--- src/main/java/singleton/App.java
+++ src/main/java/singleton/App.java
@@ -1,31 +1,19 @@
 package singleton;
 
-import java.io.*;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
 public class App {
 
-    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, IOException, ClassNotFoundException {
-        Settings settings = Settings.getInstance();
+    public static void main(String[] args) throws InvocationTargetException, InstantiationException, IllegalAccessException {
+        Settings settings = Settings.INSTANCE;
+
         Settings settings1 = null;
-
-        //싱글톤을 깨는 방법
-        //1. 리플릭션
-        //2. 직렬화 & 역직렬화
-
-        //직렬화
-        try (ObjectOutput out = new ObjectOutputStream(new FileOutputStream("settings.obj"))) {
-           out.writeObject(settings);
+        Constructor<?>[] constructors = Settings.class.getDeclaredConstructors();
+        for (Constructor<?> constructor : constructors) {
+            constructor.setAccessible(true);
+            settings1 = (Settings) constructor.newInstance("INSTANCE");
         }
-
-        //역직렬화
-        try (ObjectInput in = new ObjectInputStream(new FileInputStream("settings.obj"))) {
-            settings1 = (Settings) in.readObject();
-        }
-
         System.out.println(settings == settings1);
-
-
     }
 }
--- src/main/java/singleton/Settings.java
+++ src/main/java/singleton/Settings.java
@@ -1,46 +1,6 @@
 package singleton;
 
-import java.io.Serializable;
+public enum Settings {
+    INSTANCE
 
-public class Settings implements Serializable {
-
-    /**
-     * new를 사용해서 외부에서 객체를 생성하지 못한다.
-     */
-    private Settings() {
-    }
-
-    private static class SettingsHolder {
-        private static final Settings INSTANCE = new Settings();
-    }
-
-
-    /**
-     * 두가지 쓰레드가 동시에 if문을 접근하면 쓰레드가 두개 생성되는 문제점.
-     * 1. synchronized : 동기화 메커니즘 때문에 lock 발생 때문에 성능에 문제가 발생한다.
-     * 2. 이른 초기화(eager initialization) 사용하기
-     * - 미리 만드는게 단점
-     * - 객체 생성단계가 오래 걸리는데 만들어 놓고 사용하지 않는다면 비효율
-     * 3. 더블 체크드 락킹 사용하기
-     * - 동시에 쓰레드가 접근하더래도 if문 안에서 동기화를 처리하기 때문에 메서드에 걸리는것보다 성능의 유리
-     * - 인스턴스가필요한 시점에 생성하는점
-     * - jdk 1.5부터 동작함
-     * 4. static inner 클래스 사용하기
-     * - 권고하는 방법중 하나
-     * - 리플렉션 사용하면 문제 발생
-     * - 역직렬화
-     *
-     */
-
-    public static Settings getInstance() {
-        return SettingsHolder.INSTANCE;
-    }
-
-    /**
-     * 역직렬화시 필수로 호출되는 함수
-     * @return
-     */
-    protected Object readResolve() {
-        return getInstance();
-    }
 }
