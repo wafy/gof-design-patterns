d674ed10b9dfef2e7bb3e1ef07b4bd38323c3015
최선혁 <wafy75@gmail.com>
2022-11-19 16:01:55 +0900

    메멘토 패턴

---
+++ src/main/java/memento/before/Game.java
@@ -0,0 +1,24 @@
+package memento.before;
+
+public class Game {
+
+    private int redTeamScore;
+
+    private int blueTeamScore;
+
+    public int getRedTeamScore() {
+        return redTeamScore;
+    }
+
+    public void setRedTeamScore(int redTeamScore) {
+        this.redTeamScore = redTeamScore;
+    }
+
+    public int getBlueTeamScore() {
+        return blueTeamScore;
+    }
+
+    public void setBlueTeamScore(int blueTeamScore) {
+        this.blueTeamScore = blueTeamScore;
+    }
+}
